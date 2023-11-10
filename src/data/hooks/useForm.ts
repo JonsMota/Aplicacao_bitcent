ba604bc516ed3f9ee46bb2c0b4c444eb2d57fbbc
JonsMota <jonsmota@gmail.com>
2023-11-09 22:56:29 -0300

    criado o componente  hook useForm.ts e CRUD

---
+++ src/data/hooks/useForm.ts
@@ -0,0 +1,18 @@
+import { useCallback, useState } from "react"
+
+export default function useForm<T = any>(initialData?: T) {
+    const [data, setData] = useState<T>(initialData?? {} as T)
+
+    const changeData = useCallback(function(attribute: string, fn?: Function) {
+        return (valueOrEvent: any) => {
+            const v = valueOrEvent?.target?.value ?? valueOrEvent
+            setData({...data, [attribute]: fn?.(v) ?? v })
+        }
+    }, [data])
+
+    return {
+        data,
+        setData,
+        changeData
+    }
+}
