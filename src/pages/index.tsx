fd741ccd3a97ba51f85a19a74066caa3d0c01a7e
JonsMota <jonsmota@gmail.com>
2023-11-10 12:20:20 -0300

    criado tela de carregamento

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,12 @@ import Finances from "@/components/finances"
 import Landing from "@/components/landing"
 import AutenticacaoContext from "@/data/contexts/AutenticacaoContext"
 import { useContext } from "react"
+import Loading from "@/components/templates/Loading"
 
 export default function Home() {
-  const { usuario } = useContext(AutenticacaoContext)
+  const { usuario, carregando } = useContext(AutenticacaoContext)
+
+  if(carregando) return <Loading />
 
   return usuario ? <Finances /> : <Landing />
 }
