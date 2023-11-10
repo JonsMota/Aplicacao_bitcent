430a9e6d78cf242e7e9a0d7e82e7fc98852573f3
JonsMota <jonsmota@gmail.com>
2023-11-09 22:00:12 -0300

    configurado o componente highlight

--- src/components/landing/highlight/index.tsx
+++ src/components/landing/highlight/index.tsx
@@ -1,9 +1,20 @@
 import Area from "../common/Area"
+import Slogan from "./Slogan"
+import principal from "../../../../public/principal.jpg"
+import ResponsiveImage from "../common/ResponsiveImage"
 
 export default function Highlight() {
     return (
         <Area id="start">
-
+            <div className={`
+                h-[500px] flex items-center justify-around
+            `}>
+                <Slogan />
+                <ResponsiveImage
+                    image={principal}
+                    className="rotate-3 hidden md:inline"
+                />
+            </div>
         </Area>
     )
 }
