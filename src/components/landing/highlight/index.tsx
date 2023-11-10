de369a92d395da299739e10b857659e17ee503f9
JonsMota <jonsmota@gmail.com>
2023-11-09 21:55:07 -0300

    construcao da estrutura da landingPage

---
+++ src/components/landing/highlight/index.tsx
@@ -0,0 +1,9 @@
+import Area from "../common/Area"
+
+export default function Highlight() {
+    return (
+        <Area id="start">
+
+        </Area>
+    )
+}
---
+++ src/components/landing/testimonials/index.tsx
@@ -0,0 +1,9 @@
+import Area from "../common/Area"
+
+export default function Testimonials() {
+    return(
+        <Area>
+
+        </Area>
+    )
+}
