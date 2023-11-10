582383bcaf5fe7564f07578c4b39f8a21472416b
JonsMota <jonsmota@gmail.com>
2023-11-10 11:47:30 -0300

    configurado funcao para validar input

--- src/components/user/Forms.tsx
+++ src/components/user/Forms.tsx
@@ -1,17 +1,25 @@
 import MiniForm from "../templates/MiniForm"
 import { TextInput } from "@mantine/core"
+import falseUser from "@/data/constants/falseUser"
+import useForm from "@/data/hooks/useForm"
+import User from "@/logic/core/user/User"
+import Text from "@/logic/utils/Text"
 
 export default function Forms() {
+    const { data, changeData } = useForm<User>(falseUser)
     return (
         <div>
             <MiniForm
                 title="Seu Nome"
                 description="Como você gostaria de ser chamado?"
                 msgFooter="O nome deve possuir entre 3 e 60 caracteres, mais que isso já é um texto!"
-                canSave={true}
+                canSave={Text.between(data.name, 3, 80)}
                 save={() => {}}
             >
-                <TextInput />
+                <TextInput 
+                    value={data.name}
+                    onChange={changeData('name')}
+                />
             </MiniForm>
         </div>
     )
